3b56704134abc80001be97a8ddc7982172bda08a
thop01 <p.thong@rocmondriaan.nl>
2024-11-18 09:22:32 +0100

    querySelector & textContent geleerd

--- js/main.js
+++ js/main.js
@@ -1,1 +1,14 @@
 console.log("Het werkt");
+
+const firstname = "Pascal";
+const lastname = "Thong";
+const sex = "Male";
+let status = "online";
+let age = 34;
+
+
+const nameParagraph = document.querySelector(".name");
+const ageParagraph = document.querySelector(".age");
+
+nameParagraph.textContent = firstname + " " + lastname;
+ageParagraph.textContent = "age: " + age;
